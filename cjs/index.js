a623269b92e053d23daad697383aeb560e21c529
webreflection <andrea.giammarchi@gmail.com>
2023-06-07 07:22:33 +0200

    Less naive transfer logic

--- cjs/index.js
+++ cjs/index.js
@@ -1,6 +1,6 @@
 'use strict';
 /*! (c) Andrea Giammarchi - ISC */
-const CHANNEL = 'b7c82a36-f339-49b3-bf4f-7c8ff13de3fc';
+const CHANNEL = 'bd97610f-e514-4409-911c-e35696a48c95';
 
 // just minifier friendly for Blob Workers' cases
 const {Atomics, Int32Array, Map, SharedArrayBuffer, Uint16Array} = globalThis;
@@ -9,7 +9,11 @@ const {Atomics, Int32Array, Map, SharedArrayBuffer, Uint16Array} = globalThis;
 const {BYTES_PER_ELEMENT: I32_BYTES} = Int32Array;
 const {BYTES_PER_ELEMENT: UI16_BYTES} = Uint16Array;
 const {load, notify, store, wait} = Atomics;
+const {isArray} = Array;
 const {fromCharCode} = String;
+
+// retain buffers to trnasfer
+const buffers = new WeakSet;
 
 // retain either main threads or workers global context
 const context = new WeakMap;
@@ -17,22 +21,16 @@ const context = new WeakMap;
 // used to generate a unique `id` per each worker `postMessage` "transaction"
 let uid = 0;
 
-// used to transfer buffers instead of copying these
-let list = [];
-
 /**
  * Create once a `Proxy` able to orchestrate synchronous `postMessage` out of the box.
  * @param {globalThis | Worker} self the context in which code should run
  * @param {{parse: (serialized: string) => any, stringify: (serializable: any) => string}} [JSON] an optional `JSON` like interface to `parse` or `stringify` content
  */
 const coincident = (self, {parse, stringify} = JSON) => {
+  // create a Proxy once for the given context (globalThis or Worker instance)
   if (!context.has(self)) {
-
     // ensure the CHANNEL and data are posted correctly
-    const post = (id, sb, action, args) => self.postMessage(
-      {[CHANNEL]: {id, sb, action, args}},
-      {transfer: list.splice(0)}
-    );
+    const post = (transfer, ...args) => self.postMessage({[CHANNEL]: args}, {transfer});
 
     context.set(self, new Proxy(new Map, {
       // worker related: get any utility that should be available on the main thread
@@ -40,29 +38,31 @@ const coincident = (self, {parse, stringify} = JSON) => {
         // transaction id
         const id = uid++;
 
-        // wait until result length is known (use just i32 for simplicity)
+        // first contact: just ask for how big the buffer should be
         let sb = new SharedArrayBuffer(I32_BYTES);
         let i32a = new Int32Array(sb);
 
         // if a transfer list has been passed, drop it from args
-        if (args.at(-1) === list) args.pop();
+        let transfer = [];
+        if (buffers.has(args.at(-1) || transfer))
+          buffers.delete(transfer = args.pop());
 
-        // ask for invoke with arguments ...
-        post(id, sb, action, args);
-        // ... and wait for it
+        // ask for invoke with arguments and wait for it
+        post(transfer, id, sb, action, args);
         wait(i32a, 0);
 
         // commit transaction using the returned / needed buffer length
         const length = load(i32a, 0);
+
         // calculate the needed ui16 bytes length to store the result string
         const bytes = UI16_BYTES * length;
+
         // round up to the next amount of bytes divided by 4 to allow i32 operations
         sb = new SharedArrayBuffer(bytes + (bytes % I32_BYTES));
-        // use an i32 view to wait for results
         i32a = new Int32Array(sb);
-        // ask for results ...
-        post(id, sb);
-        // ... and wait for it
+
+        // ask for results and wait for it
+        post([], id, sb);
         wait(i32a, 0);
 
         // retrieve serialized chars and parse via known length and an ui16 view
@@ -71,9 +71,11 @@ const coincident = (self, {parse, stringify} = JSON) => {
           // TODO: find out if a push + unique fromCharCode has good old limitations/issues
           //       if not, benchmark switching over single fromCharCode(...pushed) approach
           content += fromCharCode(load(ui16a, i));
+
         // return deserialized content after previous dance to recreate it
         return parse(content);
       },
+
       // main thread related: react to any utility a worker is asking for
       set(actions, action, callback) {
         // lazy event listener and logic handling, triggered once by setters actions
@@ -84,31 +86,34 @@ const coincident = (self, {parse, stringify} = JSON) => {
           self.addEventListener('message', async ({data}) => {
             // grub the very same library CHANNEL; ignore otherwise
             const details = data?.[CHANNEL];
-            if (details) {
-              const {action, args, id, sb} = details;
+            if (isArray(details)) {
+              const [id, sb, ...rest] = details;
               // shared i32 array to unlock any worker waiting for results
               const i32a = new Int32Array(sb);
-              // no action/args means: get results out of the well known `id`
-              if (!action) {
+              // action available: it must be defined/known on the main thread
+              if (rest.length) {
+                const [action, args] = rest;
+                if (actions.has(action)) {
+                  // await for result either sync or async and serialize it
+                  const result = stringify(await actions.get(action)(...args));
+                  // store the result for "the very next" event listener call
+                  job.set(id, result);
+                  // communicate the required SharedArrayBuffer length out of the
+                  // resulting serialized string
+                  store(i32a, 0, result.length);
+                }
+                // unknown action should be notified as missing on the main thread
+                else {
+                  throw new Error(`Unsupported action: ${action}`);
+                }
+              }
+              // no action means: get results out of the well known `id`
+              else {
                 const result = job.get(id);
                 job.delete(id);
                 // populate the SaredArrayBuffer with utf-16 chars code
                 for (let ui16a = new Uint16Array(sb), i = 0; i < result.length; i++)
                   ui16a[i] = result.charCodeAt(i);
-              }
-              // action available and it must be defined/known on the main thread
-              else if (actions.has(action)) {
-                // await for result either sync or async and serialize it
-                const result = stringify(await actions.get(action)(...args));
-                // store the result for "the very next" event listener call
-                job.set(id, result);
-                // communicate the required SharedArrayBuffer length out of the
-                // resulting serialized string
-                store(i32a, 0, result.length);
-              }
-              // unknown action should be notified as missing on the main thread
-              else {
-                throw new Error(`Unsupported action: ${action}`);
               }
               // release te worker waiting to dispatch the next event
               notify(i32a, 0);
@@ -123,6 +128,6 @@ const coincident = (self, {parse, stringify} = JSON) => {
   return context.get(self);
 };
 
-coincident.transfer = (...args) => { list = args };
+coincident.transfer = (...args) => (buffers.add(args), args);
 
 module.exports = coincident;
--- test/transfer.js
+++ test/transfer.js
@@ -5,6 +5,6 @@ const {transfer} = coincident;
 
 const uInt8Array = new Uint8Array(1024 * 1024 * 8).map((v, i) => i);
 console.assert(uInt8Array.byteLength === 8388608, 'expected original length');
-until.transfer({array: uInt8Array}, transfer(uInt8Array.buffer));
+until.transfer([1, 2], {array: uInt8Array}, transfer(uInt8Array.buffer));
 console.assert(uInt8Array.byteLength === 0, 'expected transfered length');
 console.log('OK');
