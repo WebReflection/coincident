298dbf10a27942c794dc26ef03dec40381005502
webreflection <andrea.giammarchi@gmail.com>
2023-06-21 11:21:05 +0200

    Added hyperHTML test example

---
+++ test/hyperhtml.js
@@ -0,0 +1,21 @@
+import coincident from '../global.js';
+
+const {global} = coincident(self);
+
+// neededd as global in hyperHTML module
+globalThis.document = global.document;
+
+import('./hyperhtml.mjs').then(({bind}) => {
+  const html = bind(document.body);
+  tick(html);
+  setInterval(tick, 1000, html);
+});
+
+function tick(html) {
+  html`
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is ${new Date().toLocaleTimeString()}.</h2>
+    </div>
+  `;
+}
