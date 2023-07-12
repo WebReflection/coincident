23f2b9af62f7e96b17e442d1265316611c62f075
webreflection <andrea.giammarchi@gmail.com>
2023-07-12 17:37:24 +0200

    Fixed a very ugly MicroPython in WASM issue with a leaking context

--- esm/channel.js
+++ esm/channel.js
@@ -1,5 +1,5 @@
 // ⚠️ AUTOMATICALLY GENERATED - DO NOT CHANGE
-export const CHANNEL = 'c59cf82f-7b0a-4957-8c53-8f67aecc0d8e';
+export const CHANNEL = '1ac71136-6b6e-4427-96f9-571519ee49f7';
 
 export const MAIN = 'M' + CHANNEL;
 export const THREAD = 'T' + CHANNEL;
