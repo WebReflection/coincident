a0fc2a828afc0626409744a01370c560545d3204
webreflection <andrea.giammarchi@gmail.com>
2023-07-31 14:11:15 +0200

    Added iframe test case

---
+++ test/iframe/worker.js
@@ -0,0 +1,13 @@
+import coincident from '../../window.js';
+
+const {window} = coincident(self);
+const parser = new window.DOMParser;
+
+const document = parser.parseFromString(
+  '<!doctype html>',
+  'text/html'
+);
+
+document.body.textContent = 'Hello World';
+
+window.parent.postMessage(document.documentElement.outerHTML);
