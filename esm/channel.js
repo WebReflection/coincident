c440efef1e0b8dd1b58e68968082c9bd51395979
webreflection <andrea.giammarchi@gmail.com>
2023-09-27 13:31:16 +0200

    Releasing 0.13

--- esm/channel.js
+++ esm/channel.js
@@ -1,5 +1,5 @@
 // ⚠️ AUTOMATICALLY GENERATED - DO NOT CHANGE
-export const CHANNEL = '31a94196-ab71-4e10-9816-54d6502f8fab';
+export const CHANNEL = 'f4ef3141-15e3-4cfc-a4f3-d2587cfcd98a';
 
 export const MAIN = 'M' + CHANNEL;
 export const THREAD = 'T' + CHANNEL;
