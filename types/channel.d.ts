bed5256f22d3edc6f56f55f8ebb8425546be82b3
webreflection <andrea.giammarchi@gmail.com>
2023-09-27 17:17:26 +0200

    0.13.2

--- types/channel.d.ts
+++ types/channel.d.ts
@@ -1,3 +1,3 @@
-export const CHANNEL: "f4ef3141-15e3-4cfc-a4f3-d2587cfcd98a";
+export const CHANNEL: "5f50bb1c-57c3-47af-b36a-392fe80f8803";
 export const MAIN: string;
 export const THREAD: string;
