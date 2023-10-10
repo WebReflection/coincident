4a9711230fe70a65f854f2bacb5c8ec93ec56da9
webreflection <andrea.giammarchi@gmail.com>
2023-10-10 10:28:06 +0200

    Fix #26 - Avoid notifying unassigned SAB entry

--- esm/channel.js
+++ esm/channel.js
@@ -1,5 +1,5 @@
 // ⚠️ AUTOMATICALLY GENERATED - DO NOT CHANGE
-export const CHANNEL = '004aa280-89ed-4d6d-ba35-756aa263a436';
+export const CHANNEL = '8db0a6b1-4d79-4ecd-8b7a-7ea5c1811764';
 
 export const MAIN = 'M' + CHANNEL;
 export const THREAD = 'T' + CHANNEL;
