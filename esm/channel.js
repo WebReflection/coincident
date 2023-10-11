5e0c12f0afc40973683e018d964709f5bdabaef3
webreflection <andrea.giammarchi@gmail.com>
2023-10-12 00:30:44 +0200

    A better fix for the Atomics.wait with costrol value

--- esm/channel.js
+++ esm/channel.js
@@ -1,5 +1,5 @@
 // ⚠️ AUTOMATICALLY GENERATED - DO NOT CHANGE
-export const CHANNEL = '8db0a6b1-4d79-4ecd-8b7a-7ea5c1811764';
+export const CHANNEL = '0d0ac1f5-3d55-4854-86ec-2e8653dd16d4';
 
 export const MAIN = 'M' + CHANNEL;
 export const THREAD = 'T' + CHANNEL;
--- test/issue-26/worker.js
+++ test/issue-26/worker.js
@@ -3,7 +3,7 @@ console.log('worker.js');
 import coincident from '../../es.js';
 const proxy = coincident(self);
 
-(async () => {
-  for (let i = 0; i < 100000; i++)
-    proxy.func();
-})();
+for (let i = 0; i < 100000; i++)
+  proxy.func();
+
+console.log('DONE');
