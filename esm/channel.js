727bb2fe4ca0e89dfc5ae561c67ae48b08aad7a8
webreflection <andrea.giammarchi@gmail.com>
2023-10-20 17:32:04 +0200

    Updated dependencies

--- esm/channel.js
+++ esm/channel.js
@@ -1,5 +1,5 @@
 // ⚠️ AUTOMATICALLY GENERATED - DO NOT CHANGE
-export const CHANNEL = '01dab1ff-e4d3-498c-9cfe-87df993688b3';
+export const CHANNEL = '49775373-4961-44cd-9a4b-651a0c9cca5a';
 
 export const MAIN = 'M' + CHANNEL;
 export const THREAD = 'T' + CHANNEL;
