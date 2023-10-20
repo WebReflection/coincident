00ceece4392fe4773ac3cf03d4168035970ad086
webreflection <andrea.giammarchi@gmail.com>
2023-10-20 17:17:00 +0200

    0.13.11

--- esm/channel.js
+++ esm/channel.js
@@ -1,5 +1,5 @@
 // ⚠️ AUTOMATICALLY GENERATED - DO NOT CHANGE
-export const CHANNEL = '28d31fe4-f9a1-4e07-b80c-5979d3ed8c74';
+export const CHANNEL = '01dab1ff-e4d3-498c-9cfe-87df993688b3';
 
 export const MAIN = 'M' + CHANNEL;
 export const THREAD = 'T' + CHANNEL;
