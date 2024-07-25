b532d433e068dab42d9658cecd8100c9ea488730
webreflection <andrea.giammarchi@gmail.com>
2024-07-25 14:57:32 +0200

    reverted sabayon update

--- src/window/constants.js
+++ src/window/constants.js
@@ -1,4 +1,4 @@
 // ⚠️ AUTOMATICALLY GENERATED - DO NOT CHANGE
-const CHANNEL = '42fb1e9a-1373-441e-813f-357c3deaee87';
+const CHANNEL = '1351b057-ce3d-46ce-8de1-7bf1d3d63d33';
 export const MAIN = 'M' + CHANNEL;
 export const WORKER = 'W' + CHANNEL;
