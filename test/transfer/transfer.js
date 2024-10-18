d58edb9ab8ba6800fa8fb05de35cd59c0618240b
webreflection <andrea.giammarchi@gmail.com>
2024-10-18 12:42:23 +0200

    Put back the transfer test

---
+++ test/transfer/transfer.js
@@ -0,0 +1,8 @@
+import coincident from '../../dist/window/worker.js';
+const { proxy, transfer } = await coincident();
+
+const uInt8Array = new Uint8Array(1024 * 1024 * 8).map((_, i) => i);
+console.assert(uInt8Array.byteLength === 8388608, 'expected original length');
+proxy.transfer([1, 2], {array: uInt8Array}, transfer(uInt8Array.buffer));
+console.assert(uInt8Array.byteLength === 0, 'expected transfered length');
+console.log('OK');
