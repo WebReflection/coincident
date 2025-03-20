3ec8836b248e98984d08eccfbd1a0d84cc820572
webreflection <andrea.giammarchi@gmail.com>
2025-03-20 11:43:36 +0100

    updated dev/dependencies

--- src/window/constants.js
+++ src/window/constants.js
@@ -1,4 +1,4 @@
 // ⚠️ AUTOMATICALLY GENERATED - DO NOT CHANGE
-const CHANNEL = 'c70de905-a212-4eca-80aa-dad8bd4e677b';
+const CHANNEL = 'aa093abe-e9c9-4e65-ad17-7f5d34539954';
 export const MAIN = '=' + CHANNEL;
 export const WORKER = '-' + CHANNEL;
